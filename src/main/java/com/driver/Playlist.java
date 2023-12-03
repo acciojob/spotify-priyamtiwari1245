ed1a6bfbb0114f0ea62c7b00fa0b92bd279fbd34
priyamtiwari1245 <145027673+priyamtiwari1245@users.noreply.github.com>
2023-12-03 21:47:47 +0530

    Update Playlist.java

--- src/main/java/com/driver/Playlist.java
+++ src/main/java/com/driver/Playlist.java
@@ -2,32 +2,22 @@ package com.driver;
 
 import java.util.List;
 
-public class Artist {
-    private String name;
-    private int likes;
+public class Playlist {
+    private String title;
 
-    public Artist(){
+    public Playlist(){
 
     }
 
-    public Artist(String name){
-        this.name = name;
-        this.likes = 0;
+    public Playlist(String title){
+        this.title = title;
     }
 
-    public String getName() {
-        return name;
+    public String getTitle() {
+        return title;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getLikes() {
-        return likes;
-    }
-
-    public void setLikes(int likes) {
-        this.likes = likes;
+    public void setTitle(String title) {
+        this.title = title;
     }
 }
